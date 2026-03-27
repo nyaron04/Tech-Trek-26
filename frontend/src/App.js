a8c2fc9181442206c3191bdcc473801772bb0bdc
Josh Kwon <joshkwon@10-17-6-106.dynapool.wireless.nyu.edu>
2026-03-27 19:30:25 -0400

    Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Dashboard from './pages/Dashboard';
+import Landing from './components/landing'
+import './pages/Dashboard.css';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/Dashboard" element={<Dashboard/>}/>
+        <Route path="/Landing" element={<Landing/>}/>
+      </Routes>
+    </Router>
+  );
+
 }
 
 export default App;
